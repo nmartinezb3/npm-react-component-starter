59da95c0acea688c65368d08d1f9a0da22f75a18
Nicolás Martinez <nmartinez@octobot.io>
2017-09-30 16:14:43 -0300

    Initial commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/MyComponent.scss'
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <div className="my-component">
+                It is working!
+            </div>
+        )
+    }
+};
+
+export default MyComponent;
